a1c7dcafb85187b8b5b5b71705162ecc47a70435
LuKe <demon80706@gmail.com>
2026-05-25 21:17:04 +0630

    Add files via upload

---
+++ Src/API/tasks.js
@@ -0,0 +1,17 @@
+import API from "./axios";
+
+export const getTasks = async () => {
+  const res = await API.get("/tasks");
+  return res.data;
+};
+
+export const createTask = async (
+  data
+) => {
+  const res = await API.post(
+    "/tasks/create",
+    data
+  );
+
+  return res.data;
+};
