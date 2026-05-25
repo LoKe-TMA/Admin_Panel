82cbea611a6a14dbcab32300de164a423173bfc9
LuKe <demon80706@gmail.com>
2026-05-25 22:00:42 +0630

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,14 +2,27 @@ const Dashboard = () => {
   return (
     <div>
 
-      <h1 className="text-4xl font-bold mb-8">
+      <h1
+        className="
+        text-2xl md:text-4xl
+        font-bold
+        mb-8
+      "
+      >
         Dashboard
       </h1>
 
-      <div className="grid grid-cols-2 gap-5">
+      <div
+        className="
+        grid
+        grid-cols-1 md:grid-cols-2
+        gap-5
+      "
+      >
 
         <div className="bg-zinc-900 p-5 rounded-2xl">
           <h2>Total Users</h2>
+
           <p className="text-3xl mt-3">
             0
           </p>
@@ -17,6 +30,7 @@ const Dashboard = () => {
 
         <div className="bg-zinc-900 p-5 rounded-2xl">
           <h2>Total Claims</h2>
+
           <p className="text-3xl mt-3">
             0
           </p>
@@ -24,6 +38,7 @@ const Dashboard = () => {
 
         <div className="bg-zinc-900 p-5 rounded-2xl">
           <h2>Total Revenue</h2>
+
           <p className="text-3xl mt-3">
             $0
           </p>
@@ -31,6 +46,7 @@ const Dashboard = () => {
 
         <div className="bg-zinc-900 p-5 rounded-2xl">
           <h2>Online Users</h2>
+
           <p className="text-3xl mt-3">
             0
           </p>
