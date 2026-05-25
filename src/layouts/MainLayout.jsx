06a8a2fe3ce9b2a3c406030aae5b807915f3e5a4
LuKe <demon80706@gmail.com>
2026-05-25 21:55:10 +0630

    Create MainLayout.jsx

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,17 @@
+import Sidebar from "../components/Sidebar";
+
+const MainLayout = ({ children }) => {
+  return (
+    <div className="flex">
+
+      <Sidebar />
+
+      <div className="ml-64 w-full p-5">
+        {children}
+      </div>
+
+    </div>
+  );
+};
+
+export default MainLayout;
