2d3da5c45b20f6716dbdcb65a9d5bb342a1a1841
LuKe <demon80706@gmail.com>
2026-05-25 21:28:50 +0630

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://mineuc-backend.onrender.com/api"
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    "x-admin-key":
+      import.meta.env.VITE_ADMIN_KEY
+  }
 });
 
 export default API;
